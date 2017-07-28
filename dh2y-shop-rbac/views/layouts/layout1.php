5a33ba27e14346a2023a9b43081c5d039a0cd090
dailinlin <dailinlin@7477.com>
2017-07-28 19:00:13 +0800

    NavBar练习

--- dh2y-shop-rbac/views/layouts/layout1.php
+++ dh2y-shop-rbac/views/layouts/layout1.php
@@ -1,7 +1,8 @@
 <?php
-use yii;
 use yii\helpers\Html;
 use app\assets\WebAsset;
+use yii\bootstrap\NavBar;
+use yii\bootstrap\Nav;
 
 WebAsset::register($this);
 ?>
@@ -29,31 +30,45 @@ WebAsset::register($this);
 <?php $this->beginBody(); ?>
 <div class="wrapper">
     <!-- ============================================================= TOP NAVIGATION ============================================================= -->
-    <nav class="top-bar animate-dropdown">
-        <div class="container">
-            <div class="col-xs-12 col-sm-6 no-margin">
-                <ul>
-                    <li><a href="<?php echo yii\helpers\Url::to(['index/index']) ?>">首页</a></li>
-                    <?php if (\Yii::$app->session['isLogin'] == 1): ?>
-                        <li><a href="<?php echo yii\helpers\Url::to(['cart/index']) ?>">我的购物车</a></li>
-                        <li><a href="<?php echo yii\helpers\Url::to(['order/index']) ?>">我的订单</a></li>
-                    <?php endif; ?>
-                </ul>
-            </div><!-- /.col -->
-
-            <div class="col-xs-12 col-sm-6 no-margin">
-                <ul class="right">
-                    <?php if (\Yii::$app->session['isLogin'] == 1): ?>
-                        您好 , 欢迎您回来 <?php echo \Yii::$app->session['loginname']; ?> , <a
-                            href="<?php echo yii\helpers\Url::to(['member/logout']); ?>">退出</a>
-                    <?php else: ?>
-                        <li><a href="<?php echo yii\helpers\Url::to(['member/auth']); ?>">注册</a></li>
-                        <li><a href="<?php echo yii\helpers\Url::to(['member/auth']); ?>">登录</a></li>
-                    <?php endif; ?>
-                </ul>
-            </div><!-- /.col -->
-        </div><!-- /.container -->
-    </nav><!-- /.top-bar -->
+    <?php
+        NavBar::begin([
+            'options'=>[
+                'class'=>'top-bar animate-dropdown'
+            ]
+        ]);
+        echo Nav::widget([
+            'options'=>[
+                'class'=>'navbar-nav navbar-left',
+            ],
+            'items'=>[
+                ['label'=>'首页','url'=>['/index/index']],
+                \Yii::$app->session['isLogin'] == 1?(
+                ['label'=>'我的购物车','url'=>['/cart/index']]
+                ):'',
+                \Yii::$app->session['isLogin'] == 1?(
+                ['label'=>'我的订单','url'=>['/order/index']]
+                ):'',
+            ]
+        ]);
+        echo Nav::widget([
+            'options'=>[
+                'class'=>'navbar-nav navbar-right',
+            ],
+            'items'=>[
+                \Yii::$app->session['isLogin'] != 1?(
+                ['label'=>'注册','url'=>['/member/auth']]
+                ):'',
+                \Yii::$app->session['isLogin'] != 1?(
+                ['label'=>'登录','url'=>['/member/auth']]
+                ):'',
+                \Yii::$app->session['isLogin'] == 1?(
+                    '您好 , 欢迎您回来'.\Yii::$app->session['loginname'].','.
+                    Html::a('退出',['member/logout'])
+                ):'',
+            ]
+        ]);
+        NavBar::end();
+    ?>
     <!-- ============================================================= TOP NAVIGATION : END ============================================================= -->
     <!-- ============================================================= HEADER ============================================================= -->
     <header>
